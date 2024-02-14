59f07530df268151dbc563721867525315580c9d
a2623212 <a2623212@gmail.com>
2024-02-14 16:24:23 +0900

    feat: jinia template: for loop and base #120

--- flask101/app.py
+++ flask101/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -8,7 +8,18 @@ load_dotenv()
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Ally'}
+    posts = [
+        {
+            'author': {'username': 'Kenny'},
+            'body': "Japan's SAKURA is beautiful!"
+        },
+        {
+            'author': {'username': 'Mandy'},
+            'body': "My baby is quite, right?"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
 if __name__ == '__main__':
